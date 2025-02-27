a6964be5acd5f447f77927e2fb38ec09b3c78648
AneekRahman <aneek.rahman8@gmail.com>
2025-02-27 13:44:35 +1100

    Remove Dockerfile copying of model_cache

--- AWS-EC2-UserData.sh
+++ AWS-EC2-UserData.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo yum update -y
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 999009443819.dkr.ecr.us-east-1.amazonaws.com
 
-docker pull 999009443819.dkr.ecr.us-east-1.amazonaws.com/lumico/cog-flux-dev:latest
+docker pull 999009443819.dkr.ecr.us-east-1.amazonaws.com/lumico/nvidia-sana-image:latest
 
-docker run -d -p 80:5000 --gpus all 999009443819.dkr.ecr.us-east-1.amazonaws.com/lumico/cog-flux-dev:latest
+docker run -d -p 80:5000 --gpus all 999009443819.dkr.ecr.us-east-1.amazonaws.com/lumico/nvidia-sana-image:latest
